c7fb82d431ae3bf5e0a9e202ff7c47e51a43f79c
Alvaro Molina <alvmol706@alu.centroafuera.es>
2021-09-15 11:19:40 +0200

    h

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Toast.makeText(this,getString(R.string.hello), Toast.LENGTH_LONG).show()
+    Log.e("MainActivity","se ha creado correctamente")
     }
 }
